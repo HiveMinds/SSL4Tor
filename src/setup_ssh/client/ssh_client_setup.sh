61c275b923b4f2ca95e378e8fb4cc7919bbcd5d1
name <example@example.com>
2023-04-16 17:19:04 +0200

    Enabled ssh into server over onion.

--- src/setup_ssh/client/ssh_client_setup.sh
+++ src/setup_ssh/client/ssh_client_setup.sh
@@ -5,6 +5,17 @@ ssh_client_prerequisites() {
   #sudo apt install openssh-client
 
   # Connect to server
-  #ssh username@address
-  echo "TODO"
+
+  apt_remove "openssh-server" 1
+  ensure_apt_pkg "openssh-client" 1
+  ensure_apt_pkg "torsocks" 1
+
+  # THIS IS ESSENTIAL IF YOU GET:
+  # [syscall] Unsupported syscall number 39.
+  # Source: Comment in: https://askubuntu.com/q/1264335
+  # NO SUDO REQUIRED.
+  chmod g-w ~/.ssh/config
+
+  # SSH into the server:
+  torsocks ssh server_ubuntu_username@ssh_onion_domain.onion
 }
