b841a3e8da2a4b82bf2c8e67804c638044415346
name <example@example.com>
2023-04-17 14:27:51 +0200

    Automated rustup installation.

--- src/record_cli.sh
+++ src/record_cli.sh
@@ -4,7 +4,10 @@ install_cli_recording_to_gif_agg() {
   ensure_apt_pkg "curl" 1
 
   # Install build requirements for agg cargo.
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >rustup.rs
+  chmod +x rustup.rs
+  ./rustup.rs -y >>/dev/null 2>&1
+  rm rustup.rs
 
   # Get the `agg` (asciinema .cast to .gif converter)
   git clone https://github.com/asciinema/agg.git
