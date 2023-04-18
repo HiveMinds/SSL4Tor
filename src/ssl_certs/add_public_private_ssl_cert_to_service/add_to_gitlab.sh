b826f17e4e763f2a214727c859fc9df06ca94fdf
name <example@example.com>
2023-04-18 19:04:55 +0200

    Included exporting root ca into GitLab.

--- src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh
+++ src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh
@@ -7,6 +7,7 @@ source src/verification/assert_exists.sh # TODO: remove
 #SSL_PRIVATE_KEY_FILENAME="cert-key.pem"
 #SSL_PUBLIC_KEY_FILENAME="cert.pem"
 # source src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh && add_private_and_public_ssl_certs_to_gitlab "gitlab" "localhost" "cert-key.pem" "cert.pem"
+# source src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh && add_private_and_public_ssl_certs_to_gitlab "gitlab" "vwp4pobrfjwz3bsgkritleu43ptxyxybeh5hm3td7u3kzatlzq247hqd.onion" "cert-key.pem" "cert.pem"
 add_private_and_public_ssl_certs_to_gitlab() {
   local project_name="$1"
   local domain_name="$2"
@@ -26,7 +27,7 @@ add_private_and_public_ssl_certs_to_gitlab() {
   # Convert private .pem into private .key with:
   openssl pkey -in "$ssl_private_key_filepath" -out "certificates/ssl_cert/$project_name/$domain_name.key"
 
-  # Assert new local private and public certificate exist for service.
+  # Assert new local private and public certificate exist for service.Cannot issue for "localhost": Domain name needs at least one dot
   manual_assert_file_exists "certificates/ssl_cert/$project_name/$domain_name.crt"
   manual_assert_file_exists "certificates/ssl_cert/$project_name/$domain_name.key"
 
@@ -42,6 +43,31 @@ add_private_and_public_ssl_certs_to_gitlab() {
 
   manual_assert_file_exists "/etc/gitlab/ssl/$domain_name.crt"
   manual_assert_file_exists "/etc/gitlab/ssl/$domain_name.key"
+
+  # The ~/gitlab/config/gitlab.rb file says:
+  ##! Most root CA's are included by default
+  # nginx['ssl_client_certificate'] = "/etc/gitlab/ssl/ca.crt"
+  # So perhaps also include the self-signed root ca into that dir.
+  manual_assert_file_exists "certificates/root/$CA_PUBLIC_CERT_FILENAME"
+  sudo cp "certificates/root/$CA_PUBLIC_CERT_FILENAME" "/etc/gitlab/ssl/ca.cert"
+  manual_assert_file_exists "/etc/gitlab/ssl/ca.cert"
+
+  # TODO: include: in ~/gitlab/config/gitlab.rb with sudo.
+  # if [[ "$domain_name" == "localhost" ]]; then
+  #"external_url 'https://localhost'"
+  # else
+  #"external_url '$domain_name'"
+  # fi
+  # Also add to ~/gitlab/config/gitlab.rb:
+  # letsencrypt['enable'] = false
+
+  # TODO: verify the external url is found correctly:
+  #sudo cat ~/gitlab/config/gitlab.rb | grep external_url
+
+  # sudo docker ps -a
+  # e0bdc6ec75b7
+  # sudo docker exec -i e0bdc6ec75b7 bash -c "gitlab-ctl reconfigure"
+  # sudo docker exec -i e0bdc6ec75b7 bash -c "gitlab-ctl reconfigure"
 
   #sudo gitlab-ctl reconfigure
 }
