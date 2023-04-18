977d9eccc42512cbce793d637cb3793703754836
name <example@example.com>
2023-04-18 18:05:17 +0200

    Removed typo.

--- src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh
+++ src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh
@@ -3,7 +3,7 @@
 # Does not add root ca to anything.
 
 # shellcheck disable=SC1091
-source src/verification/assert_exists # TODO: remove
+source src/verification/assert_exists.sh # TODO: remove
 #SSL_PRIVATE_KEY_FILENAME="cert-key.pem"
 #SSL_PUBLIC_KEY_FILENAME="cert.pem"
 # source src/ssl_certs/add_public_private_ssl_cert_to_service/add_to_gitlab.sh && add_private_and_public_ssl_certs_to_gitlab "gitlab" "localhost" "cert-key.pem" "cert.pem"
