0975874c9d6710d656c560523f377b792a9ce1c1
name <example@example.com>
2023-05-23 18:45:44 +0200

    Reduced verbosity.

--- src/ssl_certs/add_ssl_certs_to_service/add_to_nextcloud/add_to_nextcloud.sh
+++ src/ssl_certs/add_ssl_certs_to_service/add_to_nextcloud/add_to_nextcloud.sh
@@ -18,14 +18,13 @@ add_onion_to_nextcloud_trusted_domain() {
 
   # TODO: verify format of incoming onion address.
 
-  #add Hidden Service address like a trusted domain in NextCloud instance
+  # Add Hidden Service address like a trusted domain in NextCloud instance
   # TODO: make 1 into 2 for onion domain, and 1 for localhost.
-  read -p "domain_name=$domain_name"
   sudo /snap/bin/nextcloud.occ config:system:set trusted_domains 2 --value="$domain_name"
   green_msg "The Hidden Service address has been added like trusted domain successfully."
 
   # TODO: verify output:
-  read -p "$(sudo /snap/bin/nextcloud.occ config:system:get trusted_domains)"
+  #read -p "$(sudo /snap/bin/nextcloud.occ config:system:get trusted_domains)"
 }
 
 add_certs_to_nextcloud() {
@@ -47,7 +46,6 @@ add_certs_to_nextcloud() {
   # First copy the files into nextcloud.
   # Source: https://github.com/nextcloud-snap/nextcloud-snap/issues/256
   # (see nextcloud.enable-https custom -h command).
-  #sudo cp ca.pem /var/snap/nextcloud/current/ca.pem
   sudo cp "$ssl_private_key_filepath" /var/snap/nextcloud/current/"$ssl_private_key_filename"
   sudo cp "$ssl_public_key_filepath" /var/snap/nextcloud/current/"$ssl_public_key_filename"
   sudo cp "$merged_ca_ssl_cert_filepath" /var/snap/nextcloud/current/"$merged_ca_ssl_cert_filename"
@@ -60,6 +58,4 @@ add_certs_to_nextcloud() {
   # CLI sudo /snap/bin/nextcloud.enable-https custom Says:
   sudo /snap/bin/nextcloud.enable-https custom "/var/snap/nextcloud/current/$ssl_public_key_filename" "/var/snap/nextcloud/current/$ssl_private_key_filename" "/var/snap/nextcloud/current/$merged_ca_ssl_cert_filename"
   #sudo /snap/bin/nextcloud.enable-https custom "/var/snap/nextcloud/current/cert.pem" "/var/snap/nextcloud/current/cert-key.pem" "/var/snap/nextcloud/current/fullchain.pem"
-
-  # sudo /snap/bin/nextcloud.enable-https self-signed
 }
